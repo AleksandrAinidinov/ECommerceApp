8db35a179ec129cb05fab462f5a59f483c0072ca
AleksandrAinidinov <pasulat@gmail.com>
2025-02-05 01:53:46 -0500

    Added Product.cs with all the Attributes and Methods

---
+++ ECommerceApp/Product.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceApp
+{
+    public class Product
+    {
+        public int ProdID { get; set; }
+        public string ProdName { get; set; }
+        public decimal ItemPrice { get; set; }
+        public int StockAmount { get; set; }
+
+        // Constructor
+        public Product(int prodID, string prodName, decimal itemPrice, int stockAmount)
+        {
+            ProdID = prodID;
+            ProdName = prodName;
+            ItemPrice = itemPrice;
+            StockAmount = stockAmount;
+        }
+
+        // Method to Increase stock
+        public void IncreaseStock(int amount)
+        {
+            if (amount > 0)
+                StockAmount += amount;
+        }
+
+        // Method to Decrease stock
+        public void DecreaseStock(int amount)
+        {
+            if (amount > 0 && StockAmount - amount >= 0)
+                StockAmount -= amount;
+        }
+    }
+}
